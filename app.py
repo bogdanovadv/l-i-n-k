455ca90cd54eee5a9a32af197d0b52f9f8796731
bogdanovadv <80579432+bogdanovadv@users.noreply.github.com>
2021-05-29 13:15:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ from models import *
 jwt = JWTManager(app)
 
 cors = CORS(app, resources={
-    r"/*": {"origins": Config.CORS_ALLOWED_ORIGINS}
+    r"/*": {"origins": Config.CORS_ALLOWED_ORIGINS, "Credentias", true}
 })
 
 
